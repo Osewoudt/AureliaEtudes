666fe716bb9c36ae6d77805d7abd61dd74ca5316
Bezz <dvbzz000@xs4all.nl>
2019-07-05 16:37:13 +0200

    Contact details

--- aurelia-contact-manager/contact-manager/src/contact-list.ts
+++ aurelia-contact-manager/contact-manager/src/contact-list.ts
@@ -1,19 +1,19 @@
-import { inject } from 'aurelia-dependency-injection';
-import { WebAPI } from './web-api';
-
-@inject(WebAPI)
-export class ContactList {
-  contacts;
-  selectedId = 0;
-
-  constructor(private api: WebAPI) { }
-
-  created() {
-    this.api.getContactList().then(contacts => this.contacts = contacts);
+import {WebAPI} from './web-api';
+  import {inject} from 'aurelia-framework';
+  
+  @inject(WebAPI)
+  export class ContactList {
+    contacts;
+    selectedId = 0;
+  
+    constructor(private api: WebAPI) { }
+  
+    created() {
+      this.api.getContactList().then(contacts => this.contacts = contacts);
+    }
+  
+    select(contact) {
+      this.selectedId = contact.id;
+      return true;
+    }
   }
-
-  select(contact) {
-    this.selectedId = contact.id;
-    return true;
-  }
-}
